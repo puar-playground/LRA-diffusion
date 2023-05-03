3afc2cfb2155e3bb86774c23774c36c6bf1d334d
puar <45474252+puar-playground@users.noreply.github.com>
2023-05-03 01:19:44 -0400

    Update train_Clothing1M.py

--- train_Clothing1M.py
+++ train_Clothing1M.py
@@ -148,8 +148,7 @@ if __name__ == "__main__":
     else:
         device = args.device
 
-    # data_dir = os.path.join(os.getcwd(), 'Clothing1M')
-    data_dir = os.path.join('/media/sdc/jian', 'Clothing1M_data')
+    data_dir = os.path.join(os.getcwd(), 'Clothing1M')
     print('data_dir', data_dir)
 
     n_class = 14
